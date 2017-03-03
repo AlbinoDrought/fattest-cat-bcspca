f59e095aba76829807e501186eee9c29588d336d
Sean <AlbinoDrought@users.noreply.github.com>
2017-03-03 01:52:04 -0800

    Push code, update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# fattest-cat-bcspca
-Script to find fattest cat currently up for adoption at BC SPCA 
+Script to find fattest cat currently up for adoption at BC SPCA. Port of [lexiross/fattest-cat](https://github.com/lexiross/fattest-cat).
+
+## Install
+
+Compatible with node 6+
+
+`npm install -g fattest-cat-bcspca`
+
+## Usage
+
+`$ fattest-cat-bcspca`
---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const request = require("request-promise");
+const opener = require("opener");
+const Promise = require("bluebird");
+const {compact} = require("lodash");
+const colors = require("colors");
+
+const BCSPCA_BASE = "http://adopt.spca.bc.ca";
+const ADOPTION_API = `${BCSPCA_BASE}/get.php?get=animal&cat=cats`;
+const VIEW_URL = `${BCSPCA_BASE}?profile_id=`;
+
+const fetchCats = Promise.method(() => {
+	return request({
+		uri: ADOPTION_API,
+		json: true,
+	})
+	.then((cats) => {
+		return cats;
+	})
+	.catch((err) => {
+		console.log("Error fetching cats:", err);
+		return [];
+	});
+});
+
+console.log("Accessing BC SPCA (Cat Department)...");
+
+colors.setTheme({
+	output: ["yellow", "bold"],
+});
+
+fetchCats()
+	.tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning weighing process...`))
+	.map((cat) => {
+		if(!cat.UnitWeight || cat.UnitWeight != "kg" || !cat.TotalWeight) return {};
+		
+		const name = cat.Name;
+		const weight = cat.TotalWeight.replace(cat.UnitWeight, "").trim();
+		const isFemale = cat.Sex == "Female";
+		const id = cat.ShelterBuddyID;
+		const url = VIEW_URL + id;
+		
+		console.log("Weighing cat: %s", colors.green(name));
+		
+		return { name, weight, isFemale, url };
+	})
+	.then(compact)
+	.then((cats) => {
+		let fattestCat = { weight: 0 };
+		
+		cats.forEach((cat) => {
+			if(cat.weight > fattestCat.weight) {
+				fattestCat = cat;
+			}
+		});
+		
+		console.log(`The fattest cat is ${colors.green.underline(fattestCat.name)}. ${(fattestCat.isFemale ? "She" : "He")} weighs ${fattestCat.weight} kgs.`);
+		
+		setTimeout(() => console.log("Opening cat profile..."), 2000);
+		setTimeout(() => opener(fattestCat.url), 4000);
+	});
